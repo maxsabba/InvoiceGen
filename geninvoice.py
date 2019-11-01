009f0573e0d640bc71e057e09b078f8abf6bc6d5
Massimo Sabbadini <maxsabba@gmail.com>
2019-11-01 18:56:02 +0100

    Genera delle fatture XML di test. uso: python geninvoice.py <numerodifatture>

--- geninvoice.py
+++ geninvoice.py
@@ -1,15 +1,34 @@
-# Test di estrazione e parsing di un file XML estrazione Numero e data fattura
 import xml.etree.ElementTree as ET
 import os
+from numpy import base_repr
+import random
+import sys
+
+iteraction = int(sys.argv[1])
+
 
 # apre il file scaricato dal sito fatturapa.gov.it come modello
-model = ET.parse('IT01234567890_FPA01.xml')
+invoice = ET.parse('IT01234567890_FPA01.xml')
+fn ='IT01234567890_FPA01.xml'
 
-#Estrae il progressivo invio (pi)dal file modello
-pivatr = model.find('FatturaElettronicaHeader/DatiTrasmissione/IdTrasmittente/IdCodice').text
-pi = model.find('FatturaElettronicaHeader/DatiTrasmissione/ProgressivoInvio').text
-print(pivatr, pi)
+#crea un oggetto per il tag ProgressivoInvio
+#pivatr = invoice.find('FatturaElettronicaHeader/DatiTrasmissione/IdTrasmittente/IdCodice').text
+pi = invoice.find('FatturaElettronicaHeader/DatiTrasmissione/ProgressivoInvio')
+#print(pivatr)
 
-# classe usata per la generazione del progressivo invio
+for i in range(iteraction):
+    #  generazione del progressivo invio partendo da un numero casuale
+    def calcpi():
+        new = random.randrange(1, 60466175)
+        #print('il valore è:', new)
+        new = base_repr(new, 36)       
+        #aggiunge i zeri a sx fino ad un massimo di 5 caratteri
+        new = new.rjust(5, '0')
+        return new
 
-
+    newpi = calcpi()
+    pi.text = str(newpi)
+    #pi.set('updated', 'yes')
+    newfile = fn.replace('IT01234567890_FPA01.xml', 'IT01234567890_' + str(newpi) + '.xml')
+    invoice.write('./fakexml/'+ newfile)
+    
